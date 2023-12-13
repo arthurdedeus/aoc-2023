a45449d6e3a71319bdf1df9dd1b492c534560f83
Arthur Moreira de Deus <arthur.md@parade.ai>
2023-12-13 11:44:28 -0300

    Day 9 solutions

---
+++ day9/puzzle_1.py
@@ -0,0 +1,35 @@
+lines = [line.strip().split(" ") for line in list(open("input.txt"))]
+lines = [[int(item) for item in line] for line in lines]
+
+
+def get_diffs(lines, idx, diff_map):
+    prev = lines[0]
+    diffs = []
+    for item in lines[1:]:
+        diffs.append(item - prev)
+        prev = item
+    if any(diffs):
+        diff_map[idx].append(diffs)
+        get_diffs(diffs, idx, diff_map)
+    else:
+        return
+
+
+def extrapolate(diff_map):
+    extrapolated_vals = []
+    for idx, diffs in diff_map.items():
+        aux = diffs[-1][-1]
+        for diff in reversed(diffs[:-1]):
+            aux = aux + diff[-1]
+        extrapolated_vals.append(aux)
+    return extrapolated_vals
+
+
+diff_map = {}
+for idx, line in enumerate(lines):
+    diff_map[idx] = [line]
+    get_diffs(line, idx, diff_map)
+
+extrapolated_vals = extrapolate(diff_map)
+print(sum(extrapolated_vals))
+# 2008960228
